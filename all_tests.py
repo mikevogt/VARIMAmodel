3444808e65ab010c7a192d2299036194fc211042
Michael Vogt <mike97vogt@gmail.com>
2020-05-18 21:38:30 +0200

    Update all_tests.py

--- all_tests.py
+++ all_tests.py
@@ -6,6 +6,8 @@ def test_sum():
     assert sum([1, 2, 3]) == 6, "Should be 6"
 
 def test_rMSE():
+    forecastValues = [0.0, 0.5, 0.0, 0.5, 0.0, 0.4, 0.9]
+    actualValues = [0.2, 0.4, 0.1, 0.6, 0.2, 0.1, 0.6]
     assert rootMeanSquareError(forecastValues, actualValues) == 0.24083189157584592, "Should be 0.24083189157584592"
 
 if __name__ == "__main__":
