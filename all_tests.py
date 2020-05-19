dcc3c8bdb3ac0b81ecac388b7102285ad3c3a14b
Michael Vogt <mike97vogt@gmail.com>
2020-05-19 17:27:50 +0200

    Update all_tests.py

--- all_tests.py
+++ all_tests.py
@@ -5,9 +5,6 @@ from ArimaModelGui import MyWindow
 def test_sum():
     assert sum([1, 2, 3]) == 6, "Should be 6"
 
-def test_rMSE():
-    assert rootMeanSquareError([0.0, 0.5, 0.0, 0.5, 0.0, 0.4, 0.9], [0.2, 0.4, 0.1, 0.6, 0.2, 0.1, 0.6]) == 0.24083189157584592, "Should be 0.24083189157584592"
-
 if __name__ == "__main__":
     test_sum()
     print("Everything passed")
