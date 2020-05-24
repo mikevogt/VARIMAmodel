806a292c29a257c78283800f7c5aa2037a455f10
RushilPatel0703 <30756824+RushilPatel0703@users.noreply.github.com>
2020-05-24 03:49:52 +0200

    Update test_ifStationary.py

--- test_ifStationary.py
+++ test_ifStationary.py
@@ -1,11 +1,15 @@
 import unittest
 from unittest import TestCase
 import pandas as pd
-
 from functions import ifStationary
 
-def test_ifStationary(self):
-    testData =  pd.read_csv('unit_test.csv',',')
-    a = modelFunctions()
-    result = a.ifStationary(testData)
-    self.assertEqual(result, 0)
+class TestifStationary(unittest.TestCase):
+
+    def test_ifStationary(self):
+        testData =  pd.read_csv('unit_test.csv',',')
+        a = modelFunctions()
+        result = a.ifStationary(testData)
+        self.assertEqual(result, 0)
+        
+if __name__ == '__main__':
+    unittest.main()
