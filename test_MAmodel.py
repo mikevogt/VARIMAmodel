90116a2a4e104c12a8bc5d852d672470ba2a9409
RushilPatel0703 <30756824+RushilPatel0703@users.noreply.github.com>
2020-05-25 02:55:01 +0200

    Update test_MAmodel.py

--- test_MAmodel.py
+++ test_MAmodel.py
@@ -18,7 +18,7 @@ class TestMAmodel(unittest.TestCase):
 
     def test_MAmodel(self):
         resultTestData = [-9.31472891, -32.23434828, 3.98038566, -19.73325264, -16.2288928,32.92674818, 21.60659107, -31.17029119, 40.60667671, -3.08135266,-36.39569186, 42.79855021, -40.67890097, -33.89217772, 19.28623018]
-        resultTestData = str(np.around(numpy.array(resultTestData), decimal=3))
+        resultTestData = str(np.around(numpy.array(resultTestData), decimals=3))
         testData = pd.read_csv('unit_test.csv', ',')
         diffdata = differecing(1, testData)
         result = MAmodel.MAmodel(1, diffdata)
