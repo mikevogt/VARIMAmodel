d278ee857dd4f65632dca6cc5bdbcc4e3a75e5ef
Michael Vogt <44258386+mikevogt@users.noreply.github.com>
2020-06-13 20:42:41 +0200

    GUI testing

--- testing_ArimaModelGui/Scripts/Activate.ps1
+++ testing_ArimaModelGui/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Mike\Desktop\VARIMAmodel\testing_ArimaModelGui"
+$env:VIRTUAL_ENV="C:\Users\Mike\Desktop\testing_ArimaModelGui"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
